f13b8524cb9f72a408fd3166ee7feaec62c93b6f
LeoFAndrade <leonardoferreiradeandrade15@gmail.com>
2026-08-12 17:49:59 -0300

    feat: adds task serialization to JSON and file persistence

--- task-tracker/src/main/java/com/task_tracker/repository/TaskRepository.java
+++ task-tracker/src/main/java/com/task_tracker/repository/TaskRepository.java
@@ -1,5 +1,37 @@
 package com.task_tracker.repository;
 
-public class TaskRepository {
+import com.task_tracker.model.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.io.IOException;
 
+public class TaskRepository {
+    Path path = Path.of("tasks.json");
+
+    public String toJSON(Task task) {
+        return String.format(
+                "{\"id\": %d, \"description\": \"%s\", \"status\": \"%s\", \"createdAt\": \"%s\", \"updatedAt\": \"%s\"}",
+                task.getId(), task.getDescription(), task.getStatus(), task.getCreatedAt(), task.getUpdatedAt());
+    }
+
+    public String tasksToJson(ArrayList<Task> tasks) {
+        ArrayList<String> taskList = new ArrayList<>();
+        for (Task task : tasks) {
+            taskList.add(toJSON(task));
+        }
+
+        String jsonList = String.join(",", taskList);
+        return "[" + jsonList + "]";
+    }
+
+    public void saveToJSON(ArrayList<Task> tasks) {
+        String stringTasks = tasksToJson(tasks);
+
+        try {
+            Files.writeString(path, stringTasks);
+        } catch (IOException e) {
+            System.out.println("An error occurred while modifying the filea: " + e.getMessage());
+        }
+    }
 }
