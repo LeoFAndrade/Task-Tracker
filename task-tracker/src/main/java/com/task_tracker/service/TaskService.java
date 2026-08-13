bfa4630d2e852430b641af8923b82ffebcf18fb9
LeoFAndrade <leonardoferreiradeandrade15@gmail.com>
2026-08-13 20:16:19 -0300

    feat: implement task management methods for update, delete, and status marking

--- task-tracker/src/main/java/com/task_tracker/service/TaskService.java
+++ task-tracker/src/main/java/com/task_tracker/service/TaskService.java
@@ -3,9 +3,11 @@ package com.task_tracker.service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import com.task_tracker.model.Task;
+import com.task_tracker.model.Status;
 
 public class TaskService {
     ArrayList<Task> tasks = new ArrayList<>();
+
     int nextId = 0;
 
     public void add(Task task) {
@@ -19,8 +21,64 @@ public class TaskService {
         tasks.add(task);
     }
 
+    public void update(int id, String description) {
+        Task foundTask = findTaskById(id);
+
+        if (foundTask != null) {
+            foundTask.setDescription(description);
+            foundTask.setUpdatedAt(LocalDateTime.now());
+        } else {
+            System.out.println(String.format("task-cli %d doesn't exist", id));
+        }
+    }
+
+    public void delete(int id) {
+        Task foundTask = findTaskById(id);
+
+        if (foundTask != null) {
+            tasks.remove(foundTask);
+        } else {
+            System.out.println(String.format("task-cli %d doesn't exist", id));
+        }
+    }
+
+    public void markStatus(int id, Status status) {
+        Task foundTask = findTaskById(id);
+
+        if (foundTask != null) {
+            foundTask.setStatus(status);
+            foundTask.setUpdatedAt(LocalDateTime.now());
+        } else {
+            System.out.println(String.format("task-cli %d doesn't exist", id));
+        }
+    }
+
     public ArrayList<Task> getTasks() {
         return tasks;
+    }
+
+    public ArrayList<Task> getDoneTasks() {
+        return tasks;
+    }
+
+    public ArrayList<Task> getNotDoneTasks() {
+        return tasks;
+    }
+
+    public ArrayList<Task> getInProgressTasks() {
+        return tasks;
+    }
+
+    public Task findTaskById(int id) {
+        Task foundTask = null;
+
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                foundTask = task;
+            }
+        }
+
+        return foundTask;
     }
 
 }
