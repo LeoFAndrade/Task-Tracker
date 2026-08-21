3c4447d8bfc0ce1b9acc2af10a576468e8011c2d
LeoFAndrade <leonardoferreiradeandrade15@gmail.com>
2026-08-21 17:49:19 -0300

    feat: implement update, delete, mark-status and filtered list commands

--- task-tracker/src/main/java/com/task_tracker/cli/CommandHandler.java
+++ task-tracker/src/main/java/com/task_tracker/cli/CommandHandler.java
@@ -1,6 +1,7 @@
 package com.task_tracker.cli;
 
 import com.task_tracker.model.Task;
+import com.task_tracker.model.Status;
 import com.task_tracker.service.TaskService;
 
 public class CommandHandler {
@@ -19,13 +20,65 @@ public class CommandHandler {
                 tasks.add(task);
                 break;
 
-            case "list":
-                for (Task t : tasks.getTasks()) {
-                    System.out.println(t);
-                }
+            case "update":
+                int id = Integer.parseInt(args[1]);
+                description = args[2];
+                tasks.update(id, description);
                 break;
 
-            default:                
+            case "delete":
+                id = Integer.parseInt(args[1]);
+                tasks.delete(id);
+                break;
+
+            case "list":
+
+                if (args.length > 1) {
+
+                    switch (args[1]) {
+                        case "done":
+                            for (Task t : tasks.getDoneTasks()) {
+                                System.out.println(t);
+                            }
+                            break;
+
+                        case "todo":
+                            for (Task t : tasks.getNotDoneTasks()) {
+                                System.out.println(t);
+                            }
+                            break;
+
+                        case "in-progress":
+                            for (Task t : tasks.getInProgressTasks()) {
+                                System.out.println(t);
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                } else {
+                    for (Task t : tasks.getTasks()) {
+                        System.out.println(t);
+                    }
+                }
+
+                break;
+
+            case "mark-in-progress":
+                id = Integer.parseInt(args[1]);
+                Status status = Status.IN_PROGRESS;
+                tasks.markStatus(id, status);
+                break;
+
+            case "mark-done":
+                id = Integer.parseInt(args[1]);
+                status = Status.DONE;
+                tasks.markStatus(id, status);
+                break;
+
+            default:
                 break;
         }
     }
