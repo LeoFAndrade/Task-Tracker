bb3e4b6eb6e5d5ab5658512dd1f8f2e0e3b1589e
LeoFAndrade <leonardoferreiradeandrade15@gmail.com>
2026-08-21 18:13:18 -0300

    refactor: extract JSON writing logic into JsonWriter

--- task-tracker/src/main/java/com/task_tracker/json/JsonWriter.java
+++ task-tracker/src/main/java/com/task_tracker/json/JsonWriter.java
@@ -1,5 +1,23 @@
 package com.task_tracker.json;
 
-public class JsonWriter {
+import java.util.ArrayList;
 
+import com.task_tracker.model.Task;
+
+public class JsonWriter {
+    public String toJSON(Task task) {
+        return String.format(
+                "{\"id\": %d, \"description\": \"%s\", \"status\": \"%s\", \"createdAt\": \"%s\", \"updatedAt\": \"%s\"}",
+                task.getId(), task.getDescription(), task.getStatus(), task.getCreatedAt(), task.getUpdatedAt());
+    }
+
+    public String tasksToJSON(ArrayList<Task> tasks) {
+        ArrayList<String> taskList = new ArrayList<>();
+        for (Task task : tasks) {
+            taskList.add(toJSON(task));
+        }
+
+        String jsonList = String.join(",", taskList);
+        return "[" + jsonList + "]";
+    }
 }
--- task-tracker/src/main/java/com/task_tracker/repository/TaskRepository.java
+++ task-tracker/src/main/java/com/task_tracker/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.task_tracker.repository;
 
 import com.task_tracker.json.JsonParser;
+import com.task_tracker.json.JsonWriter;
 import com.task_tracker.model.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,25 +11,11 @@ import java.io.IOException;
 public class TaskRepository {
     Path path = Path.of("tasks.json");
     JsonParser jsonParser = new JsonParser();
-
-    public String toJSON(Task task) {
-        return String.format(
-                "{\"id\": %d, \"description\": \"%s\", \"status\": \"%s\", \"createdAt\": \"%s\", \"updatedAt\": \"%s\"}",
-                task.getId(), task.getDescription(), task.getStatus(), task.getCreatedAt(), task.getUpdatedAt());
-    }
-
-    public String tasksToJSON(ArrayList<Task> tasks) {
-        ArrayList<String> taskList = new ArrayList<>();
-        for (Task task : tasks) {
-            taskList.add(toJSON(task));
-        }
-
-        String jsonList = String.join(",", taskList);
-        return "[" + jsonList + "]";
-    }
+    JsonWriter jsonWriter = new JsonWriter();
+    
 
     public void saveToJSON(ArrayList<Task> tasks) {
-        String stringTasks = tasksToJSON(tasks);
+        String stringTasks = jsonWriter.tasksToJSON(tasks);
 
         try {
             Files.writeString(path, stringTasks);
