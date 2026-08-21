cc78069ea73ff1cfb5b5ae68465fe2482f62b2ec
LeoFAndrade <leonardoferreiradeandrade15@gmail.com>
2026-08-21 17:50:13 -0300

    feat: wire up JSON persistence end-to-end in Main

--- task-tracker/src/main/java/com/task_tracker/Main.java
+++ task-tracker/src/main/java/com/task_tracker/Main.java
@@ -7,29 +7,17 @@ import java.util.ArrayList;
 import com.task_tracker.cli.CommandHandler;
 import com.task_tracker.model.Task;
 import com.task_tracker.repository.*;
-import com.task_tracker.json.JsonParser;
 
 public class Main {
     public static void main(String[] args) {
-        TaskRepository repo = new TaskRepository();
+        TaskRepository repository = new TaskRepository();
+        CommandHandler handler = new CommandHandler();
 
-        // Step 1: create some tasks and save them
-        TaskService taskService = new TaskService();
-        taskService.add(new Task("Comprar leite"));
-        taskService.add(new Task("Estudar Java"));
-        repo.saveToJSON(taskService.getTasks());
+        ArrayList<Task> loadedTasks = repository.loadFromJSON();
+        TaskService taskService = new TaskService(loadedTasks);
+        handler.execute(args, taskService);
 
-        System.out.println("Saved tasks:");
-        for (Task t : taskService.getTasks()) {
-            System.out.println(t);
-        }
-
-        // Step 2: load tasks back from the file (simulating a fresh program start)
-        System.out.println("\nLoaded tasks:");
-        ArrayList<Task> loadedTasks = repo.loadFromJSON();
-        for (Task t : loadedTasks) {
-            System.out.println(t);
-        }
+        repository.saveToJSON(taskService.getTasks());
 
     }
 }
